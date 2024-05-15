e899643ec4f3956b70e3fea231c5d38713e7cd5d
doughu <2313443705@qq,com>
2024-05-15 15:48:18 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+
+const count = ref(0)
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <!-- <button type="button" @click="count++">count is {{ count }}</button> -->
+    <van-button color="#7232dd"  plain="true" @click="count++" class="vant-button-style">count is {{ count }}</van-button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    Check out
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+.vant-button-style {
+  border: none;
+}
+
+.vant-button-style:hover {
+  border: 1px solid #888;
+}
+</style>
